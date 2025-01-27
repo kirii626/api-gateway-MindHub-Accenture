98a0da9e31e6c0ccac4ae42288e7b5760a282495
Alejandra Kiara Justiniano Olmos <kiaraalejandrajustinianoolmos@gmail.com>
2025-01-27 00:51:21 -0300

    Add basics filters

--- src/main/java/com/mindhub/eureka_client/config/GatewayConfig.java
+++ src/main/java/com/mindhub/eureka_client/config/GatewayConfig.java
@@ -16,7 +16,22 @@ public class GatewayConfig {
                 .route("products-service", r-> r.path("/products/**")
                         .uri("lb://product-microservice"))
                 .route("order-service", r -> r.path("/orders/**")
+                        .filters(f -> f.addRequestHeader("X-Origin", "Gateway"))
                         .uri("lb://order-microservice"))
+
+//               /*
+//               .route("get-user-service", r -> r.method("GET")
+//                        .and()
+//                        .path("/users/**")
+//                        .uri("lb://user-microservice"))
+//
+//                .route("write-order-service", r -> r.method("POST", "PUT", "DELETE")
+//                        .and()
+//                        .path("/orders/**")
+//                        .uri("lb://order-microservice"))
+//
+//                .route("default-route", r -> r.path("/**")
+//                        .uri("lb://product-microservice"))*/
                 .build();
     }
 }
